76f4446dd79f58b53c5d1248b1275ef1a13a6f9d
Szymono <rzkf07@gmail.com>
2023-02-14 14:46:39 +0100

    refactor: theme - css

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,22 +1,12 @@
-// .vitepress/theme/index.js
-import Layout from './LayoutX.vue'
-import DefaultTheme from 'vitepress/theme'
+import DefaultTheme from "vitepress/theme";
+import MyLayout from "./Layout.vue";
+import "./index.css";
 
 export default {
-  // root component to wrap each page
   ...DefaultTheme,
-
-  // this is a Vue 3 functional component
-  NotFound: () => 'custom 404',
-
-  enhanceApp({ app, router, siteData }) {
-    // app is the Vue 3 app instance from `createApp()`.
-    // router is VitePress' custom router. `siteData` is
-    // a `ref` of current site-level metadata.
+  Layout: MyLayout,
+  enhanceApp(ctx) {
+    DefaultTheme.enhanceApp(ctx);
+    const { app } = ctx;
   },
-
-  setup() {
-    // this function will be executed inside VitePressApp's
-    // setup hook. all composition APIs are available here.
-  }
-}
+};
