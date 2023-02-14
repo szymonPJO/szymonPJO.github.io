f768350102b005576dd4e097f248d6210d91822b
Szymono <rzkf07@gmail.com>
2023-02-14 14:48:04 +0100

    test layout

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+  import DefaultTheme from "vitepress/theme";
+  const { Layout } = DefaultTheme;
+</script>
+<style scoped></style>
+
+<template>
+    <div>
+            <h1>Hello World!</h1>
+            <p>data: {{ $frontmatter.data }}</p>
+        </div>
+    <Layout>
+        
+      <template #doc-before>
+        <h1>Hello World!</h1>
+      </template>
+      <template #doc-after>
+        <h1>Hello World!</h1>
+      </template>
+    </Layout>
+</template>
