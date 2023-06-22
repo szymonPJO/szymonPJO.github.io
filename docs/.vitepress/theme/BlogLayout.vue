c12c8bd68d1d2286cff06a08d3a5c6590937cfc0
Szymono <rzkf07@gmail.com>
2023-06-22 19:57:52 +0200

    feat: path; undefined frontamatter data warn

--- docs/.vitepress/theme/BlogLayout.vue
+++ docs/.vitepress/theme/BlogLayout.vue
@@ -4,14 +4,17 @@ const { Layout } = DefaultTheme;
 
 import { useData } from "vitepress";
 const { frontmatter } = useData();
+var dateText = frontmatter.value.date ?? "MISSED DATE !";
+var titleText = frontmatter.value.title ?? "MISSED TITLE !";
 </script>
 
 <template>
   <Layout>
     <template #doc-before>
-        <p class="date">{{ frontmatter.date}}</p>
+      <p class="path">Blog / {{ titleText }}</p>
+      <p class="date">{{ dateText }}</p>
         <div class="vp-doc">
-          <h1>{{ frontmatter.title}}</h1>
+          <h1>{{ titleText }}</h1>
         </div>
       </template>
     <template #doc-after> </template>
@@ -20,10 +23,15 @@ const { frontmatter } = useData();
 
 <style scoped></style>
 <style>
+.path {
+  font-weight: 2000;
+  font-size: xx-small;
+  opacity: 0.4;
+}
 .date {
   font-weight: 2000;
   font-size: small;
 }
-.title {
-}
+/* .title {
+} */
 </style>
