12ff4214e3944fdf47da8b87a2ea3ec871ee6d7b
Szymono <rzkf07@gmail.com>
2023-07-01 23:52:26 +0200

    feat: blog index page

---
+++ docs/.vitepress/theme/components/blogList.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { useData, withBase } from "vitepress";
+import { ref } from "vue";
+const { theme } = useData();
+let postsAll = theme.value.posts || [];
+// let posts = theme.value.posts;
+let posts = ref([]);
+let wartoscTestowa = "faesdf TEST";
+</script>
+
+<template>
+  <div class="date">{{ postsAll.length }}</div>
+  <div class="list">
+    <a class="article" v-for="post in postsAll" :href="post.regularPath">
+      <div class="[DEV]pth">PATH: {{ post.regularPath }}</div>
+      <div class="date">DATA: {{ post.frontMatter.date }}</div>
+      <div class="title">TITEL: {{ post.frontMatter.title }}</div>
+      <!-- <div class="hl">REF: {{ post.frontMatter.title }}</div> -->
+      <br />
+    </a>
+  </div>
+</template>
+
+<style>
+.page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 15px;
+}
+</style>
--- docs/src/blog/index.md
+++ docs/src/blog/index.md
@@ -1,3 +1,18 @@
+---
+layout: page
+---
+
 # Blog
 
 Wpisy o różnej tematyce lub udostępnienia treści, które uważam za wartościowe
+<BlogList/>
+
+<script setup>
+import BlogList from '@theme/components/blogList.vue'
+</script>
+
+<style>
+.h1 {
+    color: red;
+}
+</style>
